59b1ddf4ccdfd543f2acd36cee1eb1cf2a043fad
Cindy <cindy@example.com>
2026-04-15 14:06:33 -0500

    style the app

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -10,13 +10,24 @@ interface Courses {
 }
 
 const CourseList = ({courses}: Courses) => (
-    <ul>
+    <div className="grid grid-cols-[repeat(auto-fill,_minmax(200px,_1fr))] gap-4 px-4">
     {
         Object.entries(courses).map(([id, course]) => (
-            <li key={id}>{course.term} CS {course.number}: {course.title}</li>
+            <div key={id} className="flex flex-col justify-items-start h-55 w-50 p-5 m-2 border-2 border-gray-300 rounded-lg">
+                <div className="text-black text-xl p-1">
+                    {course.term} CS {course.number}
+                </div>
+                <div className="p-1 text-black text-sm flex-grow">
+                    {course.title}
+                </div>
+                <hr className="-translate-x-3 border-gray-300 w-45 my-1"/>
+                <div className="text-black text-sm p-1 m-0">
+                    {course.meets}
+                </div>
+            </div>
         ))
     }
-    </ul>
+    </div>
 );
 
 export default CourseList;
