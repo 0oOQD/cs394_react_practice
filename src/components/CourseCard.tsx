c7047ca9d2606401785f3501c9dc0d1e21a2fa29
Cindy <cindy@example.com>
2026-04-22 20:27:08 -0500

    implement selecting courses

---
+++ src/components/CourseCard.tsx
@@ -0,0 +1,34 @@
+import type { Course } from './CourseList';
+
+interface CourseCardProps {
+  courseId: string;
+  course: Course;
+  isSelected: boolean;
+  onToggle: (courseId: string) => void;
+}
+
+const CourseCard = ({ courseId, course, isSelected, onToggle }: CourseCardProps) => (
+    <div className="relative">
+        <div className={`flex flex-col justify-items-start h-55 w-50 p-5 m-2 border-2 border-gray-300 rounded-lg 
+            ${ isSelected ? 'border-blue-500 bg-blue-50' : 'border-gray-300'}
+        `}>
+            <div className="text-black text-xl p-1">
+                {course.term} CS {course.number}
+            </div>
+            <div className="p-1 text-black text-sm flex-grow">
+                {course.title}
+            </div>
+            <hr className="-translate-x-3 border-gray-300 w-45 my-1"/>
+            <div className="text-black text-sm p-1 m-0">
+                {course.meets}
+            </div>
+        </div>
+        <input 
+            type="checkbox" 
+            onChange={() => onToggle(courseId)}
+                className="absolute top-5 left-45 z-10 border-2 border-white rounded-sm checked:bg-blue-500 checked:border-blue-500" 
+        />
+    </div>
+);
+
+export default CourseCard;
--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,3 +1,6 @@
+import CourseCard from './CourseCard';
+import { useState } from 'react';
+
 export interface Course {
     term: string;
     number: string;
@@ -9,35 +12,45 @@ export interface Courses {
     courses: Record<string, Course>;
 }
 
-interface CoursesAndTerm {
+interface CoursesProps {
     courses: Record<string, Course>;
     selectedTerm: string;
 }
 
-const CourseList = ({ courses, selectedTerm }: CoursesAndTerm) => {
+const toggleList = <T,>(x: T, lst: T[]): T[] => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [...lst, x]
+);
+
+const CourseList = ({ courses, selectedTerm }: CoursesProps) => {
     const filteredCourses = Object.entries(courses).filter(
         ([_, course]) => course.term === selectedTerm
     );
 
+    const [selectedCourseIds, setSelectedCourseIds] = useState<string[]>([]);
+
+    const toggleCourse = (courseId: string) => {
+        setSelectedCourseIds(selectedCourseIds => 
+            toggleList(courseId, selectedCourseIds));
+    };
+
     return (
-        <div className="grid grid-cols-[repeat(auto-fill,_minmax(200px,_1fr))] gap-4 px-4">
-        {
-            filteredCourses.map(([id, course]) => (
-                <div key={id} className="flex flex-col justify-items-start h-55 w-50 p-5 m-2 border-2 border-gray-300 rounded-lg">
-                    <div className="text-black text-xl p-1">
-                        {course.term} CS {course.number}
-                    </div>
-                    <div className="p-1 text-black text-sm flex-grow">
-                        {course.title}
-                    </div>
-                    <hr className="-translate-x-3 border-gray-300 w-45 my-1"/>
-                    <div className="text-black text-sm p-1 m-0">
-                        {course.meets}
-                    </div>
-                </div>
-            ))
-        }
-        </div>
+        <div>
+            <div className="grid grid-cols-[repeat(auto-fill,_minmax(200px,_1fr))] gap-4 px-4 m-3">
+            {
+                filteredCourses.map(([id, course]) => (
+                    <CourseCard key={id} courseId={id} course={course}
+                        isSelected={selectedCourseIds.includes(id)} 
+                        onToggle={toggleCourse} />
+                ))
+            }
+            </div>
+            <h2 className="text-center font-black">Your Selection</h2>
+            <ul className="ml-6 h-54 overflow-auto border border-gray-400 p-4 m-5">
+            {
+            selectedCourseIds.map(id => <li key={`selectedCourse-${id}`}>{courses[id].term} CS {courses[id].number}</li>)
+            }
+        </ul>
+      </div>
     )
 };
 
